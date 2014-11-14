8a6b82c83c916f9801ebf71c012227ae55dcb49e
AWeberChrisMcGuire <chrism@aweber.com>
2014-11-14 10:43:21 -0500

    Initial commit

---
+++ sprockets/mixins/json_error/__init__.py
@@ -0,0 +1,8 @@
+"""
+mixins.json_error
+
+Handler mixin for writing JSON errors
+
+"""
+version_info = (0, 0, 0)
+__version__ = '.'.join(str(v) for v in version_info)
---
+++ tests.py
@@ -0,0 +1,13 @@
+"""
+Tests for the sprockets.mixins.json_error package
+
+"""
+import mock
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class MyTest(unittest.TestCase):
+    pass
